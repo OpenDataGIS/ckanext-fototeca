b4ce1bf2f9630ddd2d75f5b1c7cdbce473e507c4
er123451 <ehonteci@tragsa.es>
2024-05-10 13:39:48 +0200

    Arreglando archivos faltantes de commits pasados git no estaba reconociendo todos los cambios, en principio esto debería corregirlo

--- ckanext/fototeca/plugin.py
+++ ckanext/fototeca/plugin.py
@@ -15,4 +15,5 @@ class FototecaPlugin(plugins.SingletonPlugin):
     def update_config(self, config_ ):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('assets', 'ckanext-fototeca')
         
