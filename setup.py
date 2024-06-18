7144f471201f73cd5a4df1b9b2c16010ba0f7015
Ernesto Hontecillas Molina <ernestohontecillas@gmail.com>
2024-06-18 09:22:00 +0200

    actualizados namespaces rotos

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
     ],
 
 
@@ -87,7 +87,7 @@ setup(
         fototeca=ckanext.fototeca.plugin:FototecaPlugin
 
         # Harvesters
-        fototeca_pg_harvester=ckanext.fototeca.harvesters:FototecaPGHarvester
+        fototeca_sql_harvester=ckanext.fototeca.harvesters:FototecaSQLHarvester
 
         [babel.extractors]
         ckan = ckan.lib.extract:extract_ckan
